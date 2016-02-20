fb8e53c398e3e5a0719605278b2d6cf67fad8d85
Raghav Toshniwal <raghav.toshniwal@gmail.com>
2016-02-20 18:35:13 +0000

    Added chat func

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configur
 @app.route('/')
 def home():
     """Render website's home page."""
-    return render_template('home.html')
+    return render_template('chat.html')
 
 
 @app.route('/about/')
